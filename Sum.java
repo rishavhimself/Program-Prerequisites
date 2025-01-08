fe3876723042afc204496674f6724273e097fcbd
Rishav <rishavhimself@gmail.com>
2025-01-08 10:38:57 +0530

    Question 2

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Sum{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Number");
+		int a=sc.nextInt();
+		System.out.println("Enter Second Number");
+		int b=sc.nextInt();
+		System.out.println("Sum of given Numbers are : "+(a+b));
+	}
+}
