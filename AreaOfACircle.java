ecc5671a5168377ad5fa6019440fb0c39f48a1fa
Rishav <rishavhimself@gmail.com>
2025-01-08 10:39:42 +0530

    Question 4

---
+++ AreaOfACircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AreaOfACircle{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Radius");
+		Double a=sc.nextDouble();
+		System.out.println("Area of given cirlce is : "+(Math.PI*a*a));
+	}
+}
